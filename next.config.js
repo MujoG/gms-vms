a2c84fba468d3b3b577ca950ef37690677164f84
Mujo <ganjgog.business@gmail.com>
2024-03-14 11:25:20 +0100

    test3

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ require('dotenv').config();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env:{
+    DIRECTUS_URL:process.env.DIRECTUS_URL,
+  },
     images: {
         domains: ['localhost','128.140.67.194'],
       },
